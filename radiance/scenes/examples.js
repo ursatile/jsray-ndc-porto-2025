d82d1250e4e514c3ddbafd10ce43e995f4e508f1
Dylan Beattie <dylan@dylanbeattie.net>
2025-10-24 16:42:00 +0100

    boxes and planes

--- radiance/scenes/examples.js
+++ radiance/scenes/examples.js
@@ -1,5 +1,8 @@
 import { Camera, Scene, Vector, Color } from '../modules/renderer.js';
 import { Sphere } from '../modules/shapes/sphere.js';
+import { Box } from '../modules/shapes/box.js';
+import { Plane } from '../modules/shapes/plane.js';
+
 import { Light } from '../modules/light.js';
 import { Appearance } from '../modules/appearance.js';
 
@@ -10,18 +13,20 @@ export function EmptySky() {
 }
 
 export function ColoredSpheres() {
-    let camera = new Camera(new Vector(0, 2, -8), Vector.Z);
-    let background = Color.Black;
+    let camera = new Camera(new Vector(2, 4, -8), Vector.Z);
+    let background = new Color(100,120,240);
     let shapes = [
-        new Sphere(new Vector(-4, 0, 4), 1, Color.Yellow),
-        new Sphere(new Vector(-2, 0, 2), 1, Color.Red),
-        new Sphere(new Vector(+0, 0, 0), 1, Color.White),
-        new Sphere(new Vector(+2, 0, 2), 1, Color.Green),
-        new Sphere(new Vector(+4, 0, 4), 1, Color.Blue),
+        new Sphere(new Vector(-4, 1, 4), 1, Color.Yellow),
+        new Sphere(new Vector(-2, 1, 2), 1, Color.Red),
+        // new Sphere(new Vector(+0, 1, 0), 1, Color.White),
+        new Box(new Vector(-1,0,-1), new Vector(1,2,1), Color.Magenta),
+        new Sphere(new Vector(+2, 1, 2), 1, Color.Green),
+        new Sphere(new Vector(+4, 1, 4), 1, Color.Blue),
+        new Plane((Vector.Y), 0, Color.White)
     ];
     let lights = [ 
         new Light(new Vector(5, 10, -5), Color.White),
-        new Light(new Vector(-5, 2, -5), new Color(255,127,50)),
+        // new Light(new Vector(-5, 2, -5), new Color(255,127,50)),
      ]; 
     return new Scene(camera, background, shapes, lights);
 }
